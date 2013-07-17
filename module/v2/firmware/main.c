a23e74565499040dbdeece9dd0e32144ba4d7f17
Paulo <pacarvalho@wpi.edu>
2013-07-17 20:05:52 -0300

    new module code directly in avr C

---
+++ module/v2/firmware/main.c
@@ -0,0 +1,18 @@
+/* Name: main.c
+ * Author: Joao Soares, Paulo Carvalho
+ * Copyright: Someday...
+ * License: None
+ */
+
+/* Firmware for a SPI capable LED-Test module. Modules use the AHComm-V1 protocol*/
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* insert your hardware initialization here */
+    for(;;){
+        /* insert your main loop code here */
+    }
+    return 0;   /* never reached */
+}
