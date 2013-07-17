047bd6a778b451ba0875be3a48de465206246b81
Paulo <pacarvalho@wpi.edu>
2013-07-17 20:41:34 -0300

    Initial Push of main.c
    Started program

--- module/v2/firmware/main.c
+++ module/v2/firmware/main.c
@@ -10,9 +10,41 @@
 
 int main(void)
 {
-    /* insert your hardware initialization here */
+    /* Initialize the module and internal variables */
+    /* Setup pin directions and interrupts */
+    thismodule.setup();
+    /* Setup SPI communication */
+	ahcomm.setup();
+
     for(;;){
-        /* insert your main loop code here */
+        /* Are there serial packets in buffer? */
+        if (ahcomm.packetAvailable())
+        {
+        	/* Create array with size of packet to be read */
+        	uint8_t packet[ahcomm.packetSize()];
+        	/* Read the body of the packet into packet array */
+        	ahcomm.readPacket(packet, ahcomm.packetSize());
+
+        	/* Update system status with information from packet */
+        	thismodule.actOn(packet);
+
+        	/* If requested, return a status query */
+        	if (thismodule.statusRequested())
+        	{
+        		/* Create array to store body of status packet */
+        		uint8_t array[thismodule.sizeOfPacket()];
+        		/* Saves into array the module's current status */
+        		thismodule.getStatus(array);
+        		/* Packages and sends body through SPI */
+        		ahcomm.transmit(array);
+        	}
+        }
     }
     return 0;   /* never reached */
+}
+
+/* Interrupt routine for receiving SPI data */
+ISR (SPI_STC_vect)
+{
+	ahcomm.receive(SPDR);
 }
